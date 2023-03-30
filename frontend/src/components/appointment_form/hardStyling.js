81f01c4c2ecbda1ac992b25059ae7a24c7ad884d
Bhupender Soni <official.soni532@gmail.com>
2023-03-30 19:31:07 +0530

    created hardStyling for overide the library styles

---
+++ frontend/src/components/appointment_form/hardStyling.js
@@ -0,0 +1,24 @@
+const styles = {
+    input: {
+        padding: '15px 25px',
+        lineHeight: '24px',
+        /* border: 2px solid #91b3fa, */
+        border: '2px solid #e1e9fe',
+        borderRadius: '1rem',
+        width: '100%',
+        fontSize: '18px',
+        color: 'rgb(85, 85, 85)',
+        background:'white',
+        '&::placeholder':{
+            color:'red'
+        },
+        resize: 'none'
+    },
+    formBtn:{
+        background: '#4547ed',
+        fontWeight: 600,
+        
+    }
+}
+
+export default styles;
