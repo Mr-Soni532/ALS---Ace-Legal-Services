1a067f99b455cca206891853c8c8f64ac9a199e0
Bhupender Soni <official.soni532@gmail.com>
2023-04-01 00:04:20 +0530

    modified bookSlot page

--- frontend/src/pages/BookSlot/BookSlot.jsx
+++ frontend/src/pages/BookSlot/BookSlot.jsx
@@ -1,25 +1,57 @@
-import React, { useState } from 'react'
+import React, {  useContext, useEffect, useState } from 'react'
 import Calander from '../../components/slots/Calander'
 import SlotBtn from '../../components/slots/SlotBtn';
+import { AppointmentContext } from '../../context/appointment/appointmentContext';
 import './bookSlot.style.css'
 
 const BookSlot = () => {
+    const context = useContext(AppointmentContext)
+    const {setDetials} = context;
     const [inputDate, setInputDate] = useState("");
+    const [availableSlot, setAvailableSlot] = useState([true,false,true,false]);
     function updateDate(date){
         setInputDate(date)
+        setTimeout(() => {
+            
+        }, 0);
     }
+    let appointmentData = []
+    function getAvailableSlots(){
+        let selectedDate = inputDate.split("-").map(Number) //YYYY-MM-DD
+        let appointmentByYear = appointmentData.filter(el => el.year === selectedDate[0])
+        let appointmentByMonth = appointmentByYear.filter(el => el.month === selectedDate[1])
+        let appointmentByDay = appointmentByMonth.filter(el => el.day === selectedDate[2])
+        let newArr = new Array(4).fill(true)
+        appointmentByDay.forEach(el =>{
+            newArr[el.slot-1] = false;
+        })
+        setAvailableSlot(newArr)
+    }
+
+    // updating the appointment context
+    useEffect(() => {
+      setDetials(prev => ({
+        ...prev, ...{date:inputDate}
+      }))
+    }, [inputDate,setDetials])
+
+    function api(){
+
+    }
+    api()
+
   return (
     <div className='slot_mainContainer'>
         <div className="slot_col-1">
             <Calander updateDate={updateDate}/>
         </div>
         <div className="slot_col-2">
-            <span>Available Slots {inputDate}</span>
+            <span>Select Slot</span>
             <div className="slot_container">
-                <SlotBtn time={'9:00 AM'} active={true}/>
-                <SlotBtn time={'11:00 AM'} active={false}/>
-                <SlotBtn time={'2:00 PM'} active={false}/>
-                <SlotBtn time={'4:00 PM'} active={true}/>
+                <SlotBtn time={'9:00 AM'} active={availableSlot[0]} slot={1}/>
+                <SlotBtn time={'11:00 AM'} active={availableSlot[1]} slot={2}/>
+                <SlotBtn time={'2:00 PM'} active={availableSlot[2]} slot={3}/>
+                <SlotBtn time={'4:00 PM'} active={availableSlot[3]} slot={4}/>
             </div>
         </div>
     </div>
