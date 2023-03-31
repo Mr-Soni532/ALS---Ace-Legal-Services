df94073c8a332f21671a2ca52efc2ae53f12e2ab
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2023-03-31 13:33:14 +0530

    changed all routs to app-js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-import Allroutes from './components/Allroutes';
+import { Route, Routes } from 'react-router-dom';
+import IndexPage from './pages/Index_Page/IndexPage';
+import Appointment from './pages/appointment/Appointment';
+import Lawyers from './pages/Lawyer/Lawyers';
+import UserDashboard from './pages/User_Dashboard/UserDashboard';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 function App() {
   return (
     <div className="App">
-      <Allroutes />
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/lawyers" element={<Lawyers />} />
+        <Route path="/userdashboard" element={<UserDashboard />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div >
   );
 }
