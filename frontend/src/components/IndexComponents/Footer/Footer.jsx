3b39c3be0e5f9e74d0c049985b2d6435f91d443d
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2023-03-31 09:29:58 +0530

    footerindex

--- frontend/src/components/IndexComponents/Footer/Footer.jsx
+++ frontend/src/components/IndexComponents/Footer/Footer.jsx
@@ -4,8 +4,28 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="FooterParent">
-      <div className="Footer1">Ace Logo ace email</div>
-      <div className="Footer2">acce adsfasdk</div>
+      <div className="Footer1">
+        <div>
+          <img
+            style={{ width: "280px" }}
+            src="Images/FooterImages/ACEyellow.png"
+            alt=""
+          />
+          <br />
+          <br />
+        </div>
+        <div>
+          <p className="footerpara">
+            © 2023 <span className="footergoldenpara">Ace Legal Group</span>{" "}
+            <br /> Designed and Developed by <br />
+            <span className="footergoldenpara">young zinc group</span>
+          </p>
+        </div>
+      </div>
+      <div className="Footer2">
+        <p className="footeremail">info@acelegalservices.com</p>
+        <p>Contact Us: +0781 8462 8462</p>
+      </div>
     </div>
   );
 };
--- frontend/src/components/IndexComponents/Section9/Section9.jsx
+++ frontend/src/components/IndexComponents/Section9/Section9.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Section9 = () => {
   return (
     <div>
-      <h1>Get To know us More</h1>
+      <h1 className="SectionHeads">Get To know us More</h1>
+      <div className="SecNineParent">
+        <div className="YTvideoBox">ytvideo</div>
+        <div className="RequestCons">form</div>
+      </div>
     </div>
   );
 };
