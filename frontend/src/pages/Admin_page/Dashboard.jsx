aab05e012cf3499db6ddcf2d68af390694ab777a
unknown <mohimabahadur@gmail.com>
2023-04-01 15:03:40 +0530

    admin pages added

---
+++ frontend/src/pages/Admin_page/Dashboard.jsx
@@ -0,0 +1,13 @@
+import { Space, Card, Statistic, Typography } from "antd"
+import React from "react";
+import "../../utlis/admin_css/content.css";
+import Headers from "../../component/AdminCompo/Headers";
+
+const  Dashborad = ()=>{
+    return <div>
+        <Headers/>
+        <div>Dashboard</div>        
+    </div>
+}
+
+export default Dashborad;
