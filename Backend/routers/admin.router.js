b1f690da1e030e7757aa69ec3b2da49967ecd833
Bhupender Soni <official.soni532@gmail.com>
2023-04-01 17:06:24 +0530

    Modified admin router

--- Backend/routers/admin.router.js
+++ Backend/routers/admin.router.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const AdminRouter = express.Router();
 
+AdminRouter.get('/getAllLawyers', )
+AdminRouter.get('/getAllUsers', )
 
+// Lawyer
+AdminRouter.get('/getLawyer/:id', )
+AdminRouter.post('/addLawyer', )
+AdminRouter.delete('/deleteLawyer/:id', )
+
+// Lawyer
+AdminRouter.get('/getUser/:id', )
+AdminRouter.post('/addUser', )
+AdminRouter.delete('/deleteUser/:id', )
 
 
 module.exports = AdminRouter;
