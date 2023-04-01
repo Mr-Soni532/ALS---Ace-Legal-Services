9db86a0351149f66a4f9d237523785790663d381
Bhupender Soni <official.soni532@gmail.com>
2023-04-01 19:16:46 +0530

    modified index structure

--- Backend/index.js
+++ Backend/index.js
@@ -5,74 +5,38 @@ const UserRouter = require('./routers/user.router');
 const LawyerRouter = require('./routers/lawyer.router');
 const AdminRouter = require('./routers/admin.router');
 const app = express();
-const passport=require("./config/google.auth");
-// const UserSchema = require('./model/user.model');
+const passport = require("./config/google.auth");
 const cookieSession = require("cookie-session");
-
-require('dotenv').config()
-
-const PORT = process.env.PORT;
+const AuthRouter = require('./routers/auth.router');
 
 //=============> ENV VARIABLES
+require('dotenv').config()
+const PORT = process.env.PORT;
+
+//=============> MIDDLEWARES
 app.use(express.json())
 app.use(cors())
+app.use(
+  cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //=============> Testing endpoint
 app.get('/', (req, res) => res.send({ Message: 'ALS server working fine' }))
 
 //=============> ROUTES
-app.use(
-  cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
-);
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-
-app.use('/user',UserRouter)
-app.use('/lawyer',LawyerRouter)
-app.use('/admin',AdminRouter)
-app.get("/auth/login/success", (req, res) => {
-  if (req.user) {
-    res.status(200).json({
-      success: true,
-      message: "successfull",
-      user: req.user,
-      //   cookies: req.cookies
-    });
-  }
-});
-
-
-app.get("/auth/login/failed", (req, res) => {
-  res.status(401).json({
-    success: false,
-    message: "failure",
-  });
-});
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile',"email"] }));
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login',session:false }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('http://localhost:3000/userdashboard');
-  });
-
-
-
-
-
+app.use('/user', UserRouter)
+app.use('/lawyer', LawyerRouter)
+app.use('/admin', AdminRouter)
+app.use('/auth', AuthRouter)
 
 //=============> CONNECTION
-app.listen(PORT, async (err) => {
-    try {
-        await connectToMongo();
-        console.log(`ALS backend running @ ${PORT}`)
-    } catch (error) {
-        console.log(error)
-    }
+app.listen(PORT, async () => {
+  try {
+    await connectToMongo();
+    console.log(`ALS backend running @ ${PORT}`)
+  } catch (error) {
+    console.log(error)
+  }
 })
