2ffc6c6153a260c0683383f0c64b6ad23ca05986
Bhupender Soni <official.soni532@gmail.com>
2023-04-03 11:31:26 +0530

    changed baseURL

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,3 +1,3 @@
 // const HOST = 'http://localhost:4000'
-const HOST = 'young-zinc-6102-562h-mr-soni532.vercel.app'
+const HOST = 'https://young-zinc-6102-562h-mr-soni532.vercel.app'
 export default HOST 
